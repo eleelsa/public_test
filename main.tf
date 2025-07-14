3b842819ac34ac97dde107a1487d53130343f43b
otsuka <ryuta_otsuka@crosshead.co.jp>
2025-07-14 12:03:15 +0900

    json 化

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block                           = "10.0.0.0/16"
-  assign_generated_ipv6_cidr_block     = false
-  enable_dns_hostnames                 = false
-  enable_network_address_usage_metrics = false
-  enable_dns_support                   = true
-  instance_tenancy                     = "default"
-}
